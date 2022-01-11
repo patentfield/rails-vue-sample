3829a247ab04095a349c643c90b57418ea6b4378
naoa <visionary.incident@gmail.com>
2022-01-11 15:30:03 +0900

    Fix asset path proxy

--- lib/tasks/assets_path_proxy.rb
+++ lib/tasks/assets_path_proxy.rb
@@ -4,27 +4,22 @@ class AssetsPathProxy < Rack::Proxy
   include ActionView::Helpers::AssetUrlHelper
   def perform_request(env)
     request = Rack::Request.new(env)
-    if request.path =~ /hot-update\.(js|json)/
-      if Rails.env.development?
-        dev_server = env["HTTP_HOST"].gsub(":3000", ":3035")
-        env["HTTP_HOST"] = dev_server
-        env["HTTP_X_FORWARDED_HOST"] = dev_server
-        env["HTTP_X_FORWARDED_SERVER"] = dev_server
-        env["PATH_INFO"] = "/public/assets/" + env["PATH_INFO"].split("/").last
-      end
+    if Rails.env.development? && request.path =~ /hot-update\.(js|json)/
+      dev_server = env["HTTP_HOST"].gsub(":3000", ":3035")
+      env["HTTP_HOST"] = dev_server
+      env["HTTP_X_FORWARDED_HOST"] = dev_server
+      env["HTTP_X_FORWARDED_SERVER"] = dev_server
+      env["PATH_INFO"] = "/public/assets/" + env["PATH_INFO"].split("/").last
       super
-    elsif env["PATH_INFO"].include?("/images/") && !request.path.end_with?(*[".php", ".pl", ".zip", ".rar", ".asp", "admin1", "sysadmins", "src_images_but_dianz_s.png"])
-      if Rails.env.development?
-        dev_server = env["HTTP_HOST"].gsub(":3000", ":3035")
-        env["HTTP_HOST"] = dev_server
-        env["HTTP_X_FORWARDED_HOST"] = dev_server
-        env["HTTP_X_FORWARDED_SERVER"] = dev_server
-      end
-      if Rails.env.development?
-        env["PATH_INFO"] = "/public/assets/images/" + env["PATH_INFO"].split("/").last
-      elsif Rails.env.test?
-        env["PATH_INFO"] = asset_path("/assets-test/images/" + env["PATH_INFO"].split("/").last)
-      end
+    elsif Rails.env.development? && env["PATH_INFO"].include?("/images/")
+      dev_server = env["HTTP_HOST"].gsub(":3000", ":3035")
+      env["HTTP_HOST"] = dev_server
+      env["HTTP_X_FORWARDED_HOST"] = dev_server
+      env["HTTP_X_FORWARDED_SERVER"] = dev_server
+      env["PATH_INFO"] = "/public/assets/images/" + env["PATH_INFO"].split("/").last
+      super
+    elsif Rails.env.test? && env["PATH_INFO"].include?("/images/")
+      env["PATH_INFO"] = asset_path("/assets-test/images/" + env["PATH_INFO"].split("/").last)
       super
     else
       @app.call(env)
